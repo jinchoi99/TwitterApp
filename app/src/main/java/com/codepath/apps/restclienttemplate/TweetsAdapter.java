d3bf9029547750863b5ad3d23ac1d95909e69f3f
Jinny Choi <jinchoi99@fb.com>
2020-06-30 05:11:21 -0700

    Create TweetsAdapter

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -0,0 +1,12 @@
+package com.codepath.apps.restclienttemplate;
+
+public class TweetsAdapter {
+    //Pass in the context and list of tweets
+
+    //For each row, inflate the layout
+
+    //Bind values based on the position of the element
+
+    //Define a viewholder
+    
+}
