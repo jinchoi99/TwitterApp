560b1dd4d8db7ea592fa839f7ad05637a29c313a
Jinny Choi <jinchoi99@fb.com>
2020-06-30 05:15:03 -0700

    Define ViewHolder

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,5 +1,12 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class TweetsAdapter {
     //Pass in the context and list of tweets
 
@@ -8,5 +15,17 @@ public class TweetsAdapter {
     //Bind values based on the position of the element
 
     //Define a viewholder
-    
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+
+        //itemView represents one row in the recycler view = a single tweet
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+        }
+    }
 }
